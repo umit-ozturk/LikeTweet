4c8b61c4e17af4282f232aa695b740c5460e0642
Ümit <umit.ozturk.9716@gmail.com>
2017-11-16 23:19:26 +0300

    First CRUD Operation

--- src/liketweet/urls.py
+++ src/liketweet/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,8 +22,9 @@ from .views import home
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name="home"),
+    url(r'^tweet/', include('tweets.urls')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG: 
 	#urlpatterns += ((static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
