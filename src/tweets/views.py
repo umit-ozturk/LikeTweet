7e76dab4575c1dc5760dd4ba5010f83cadce9d40
Ümit <umit.ozturk.9716@gmail.com>
2017-11-17 00:19:45 +0300

    Class Based Views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic  import DetailView, ListView
 from .models import Tweet
 # Create your views here.
 
@@ -14,23 +14,18 @@ from .models import Tweet
 
 
 
-
-### Functional based views. Actually functional based views not usefull. Therefore we will write Class Based Views at the next time.
-def tweet_detail_view(request, id=1):
-	obj = Tweet.objects.get(id=id) #Get from DATABASE
-	print(obj)
-	context = {
-		"object" : obj
-	}
-	return render(request, "tweets/detail_view.html", context)
-
-
-def tweet_list_view(request):
+# Defaul Template Path "tweets/detail_view.html"
+class TweetDetailView(DetailView):
 	queryset = Tweet.objects.all()
-	print(queryset)
-	for obj in queryset:
-		print(obj.content)
-	context = {
-		"object_list" : queryset
-	}
-	return render(request, "tweets/list_view.html", context)
+
+	def get_object(self):
+		return Tweet.objects.get(id=1)
+
+
+class TweetListView(ListView):
+	queryset = Tweet.objects.all()
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(TweetListView, self).get_context_data(*args, **kwargs)
+		### print(context) --> We see tweet data.
+		return context
