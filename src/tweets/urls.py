43b93bf37b8f9e69a95e3f78baaf443a846511da
Ümit <umit.ozturk.9716@gmail.com>
2017-11-21 19:06:43 +0300

    Delete View

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import  (
 	TweetCreateView,
+	TweetDeleteView,
 	TweetDetailView,
 	TweetListView,
 	TweetUpdateView
@@ -11,6 +12,7 @@ urlpatterns = [
     url(r'^create/$', TweetCreateView.as_view(), name="create"), #/tweet/create
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"), #/tweet/1
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name="update"), #/tweet/1/update
+    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name="delete"), #/tweet/1/delete
 ]
 
 
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin # Dont using now
 from django.shortcuts import render, get_object_or_404
-from django.views.generic  import DetailView, ListView, CreateView, UpdateView
-
+from django.urls import reverse_lazy
+from django.views.generic  import (
+			CreateView,
+			DetailView, 
+			DeleteView,
+			ListView,
+			UpdateView
+			)
 from .forms import TweetModelForm
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
-# Create your views here.
-
 
 #Create
 class TweetCreateView(FormUserNeededMixin, CreateView): #LoginRequiredMixin importing class auto authenticated validation
@@ -14,40 +18,30 @@ class TweetCreateView(FormUserNeededMixin, CreateView): #LoginRequiredMixin impo
 	template_name = "tweets/create_view.html"
 	success_url = "/tweet/create/" 
 	#login_url = "/admin/". # About LoginRequiredMixin redirect to admin 
-
-
-
 #Update
-
 class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
 	queryset = Tweet.objects.all()
 	form_class  = TweetModelForm
 	template_name = "tweets/update_view.html"
 	success_url = "/tweet/"
-
-
-
-
 #Delete
-
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+	model = Tweet
+	template_name = "tweets/delete_confirm.html"
+	success_url = reverse_lazy("home")
 #Retrieve
-
-
-
-# Defaul Template Path "tweets/detail_view.html"
 class TweetDetailView(DetailView):
 	queryset = Tweet.objects.all()
-
-	#pk = self.kwargs.get("pk")
-
-	#def get_object(self):
-	#	return Tweet.objects.get(id=pk )
-
-
+#List
 class TweetListView(ListView):
 	queryset = Tweet.objects.all()
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(TweetListView, self).get_context_data(*args, **kwargs)
-		### print(context) --> We see tweet data.
 		return context
+		
+def tweet_detail_view(request, pk=None):
+	obj = get_object_or_404(Tweet, pk=pk)
+	context = {
+		"object" : obj
+	}
