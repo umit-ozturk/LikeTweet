566a5adb4e6445d509c0781aa537af25d6859764
Ümit <umit.ozturk.9716@gmail.com>
2017-11-21 16:01:31 +0300

    Model Form and Simple Validation

---
+++ src/tweets/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .models import Tweet
+
+
+class TweetModelForm(forms.ModelForm):
+	class Meta:
+		model = Tweet
+
+		fields = [
+			"user",
+			"content"
+		]
+
+
+	def clean_content(self, *args, **kwargs):
+		content = self.cleaned_data.get("content")
+		if content == "abc":
+			raise forms.ValidationError("Cannot be abc")
+		return content
