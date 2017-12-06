8de1c186fd6bc734e71535df0644c0eced65dfad
Ümit <umit.ozturk.9716@gmail.com>
2017-12-06 18:29:55 +0300

    Rest API Serializer and UserDisplaySerializer

--- src/liketweet/settings/base.py
+++ src/liketweet/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'rest_framework',
-    
+    'accounts',
     'tweets',
 ]
 
---
+++ src/tweets/api/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+
+from django.views.generic.base import RedirectView
+
+from .views import  (
+	TweetListAPIView
+#	TweetCreateView,
+#	TweetDeleteView,
+#	TweetDetailView,
+#	TweetListView,
+#	TweetUpdateView
+	)
+
+urlpatterns = [
+#	url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^$', TweetListAPIView.as_view(), name="list"), #/tweet/api/
+#    url(r'^create/$', TweetCreateView.as_view(), name="create"), #/tweet/create
+#    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name="detail"), #/tweet/1
+#    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name="update"), #/tweet/1/update
+#    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name="delete"), #/tweet/1/delete
+]
+
+
