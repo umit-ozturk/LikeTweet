8f24e653e4d4a684bab7982a88a3f941ab9f4a30
Ümit <umit.ozturk.9716@gmail.com>
2018-01-03 11:53:00 +0300

    Created Parent Tweet Model Serializer for Retweet

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -6,12 +6,35 @@ from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet
 
 
+class ParentTweetModelSerializer(serializers.ModelSerializer):
+	user = UserDisplaySerializer(read_only=True)
+	date_display = serializers.SerializerMethodField()
+	timesince = serializers.SerializerMethodField()
+
+
+	class Meta:
+		model = Tweet
+		fields = [
+			'id',
+			'user',
+			'content',
+			'timestamp',
+			'date_display',
+			'timesince'
+		]
+
+	def get_date_display(self, obj):
+		return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
+
+	def get_timesince(self, obj):
+		return timesince(obj.timestamp) + "ago"
+
 
 class TweetModelSerializer(serializers.ModelSerializer):
 	user = UserDisplaySerializer(read_only=True)
 	date_display = serializers.SerializerMethodField()
 	timesince = serializers.SerializerMethodField()
-	is_retweet = serializers.SerializerMethodField()
+	parent = ParentTweetModelSerializer()
 
 
 	class Meta:
@@ -23,13 +46,9 @@ class TweetModelSerializer(serializers.ModelSerializer):
 			'timestamp',
 			'date_display',
 			'timesince',
-			'is_retweet'
+			'parent'
 		]
 
-	def get_is_retweet(self, obj):
-		if obj.parent:
-			return True
-		return False
 
 	def get_date_display(self, obj):
 		return obj.timestamp.strftime("%b %d, %Y at %I:%M %p")
