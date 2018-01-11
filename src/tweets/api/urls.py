9a7c1882d328fce93f02935ad03b6944fc60ec70
Ümit <umit.ozturk.9716@gmail.com>
2018-01-12 02:46:05 +0300

    Revert "Like Button Create Model, API Endpoint and Retweet Button Bug Fix"
    This reverts commit 1b07a4dacc9baf7993ba88e693769af76bb322fb.

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -3,17 +3,20 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import  (
-	LikeToggleAPIView,
 	RetweetAPIView,
 	TweetCreateAPIView,
-	TweetListAPIView,
+	TweetListAPIView
+#	TweetCreateView,
+#	TweetDeleteView,
+#	TweetDetailView,
+#	TweetListView,
+#	TweetUpdateView
 	)
 
 urlpatterns = [
 #	url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name="list"), #/api/tweet
     url(r'^create/$', TweetCreateAPIView.as_view(), name="create"), #/tweet/create
-    url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name="like-toggle"),
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name="retweet"), #/api/tweet/id/retweet
 #    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name="update"), #/tweet/1/update
 #    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name="delete"), #/tweet/1/delete
