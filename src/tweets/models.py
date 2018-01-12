c18c405cbf021c3ebb91b858ff126e7a049e7024
Ümit <umit.ozturk.9716@gmail.com>
2018-01-12 03:07:21 +0300

    Like Button Create Model, API Endpoint and Retweet Button Bug Fix

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -36,10 +36,20 @@ class TweetManager(models.Manager):
 		obj.save()
 		return obj
 
+	def like_toggle(self, user, tweet_obj):
+		if user in tweet_obj.liked.all():
+			is_liked = False
+			tweet_obj.liked.remove(user)
+		else:
+			is_liked = True
+			tweet_obj.liked.add(user)
+		return is_liked
+
 class Tweet(models.Model):
 	parent		= models.ForeignKey("self", blank=True, null=True)
 	user 		= models.ForeignKey(settings.AUTH_USER_MODEL)
 	content 	= models.CharField(max_length=140, validators=[validate_content])
+	liked		= models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked')
 	updated 	= models.DateTimeField(auto_now=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 
@@ -68,4 +78,4 @@ def tweet_save_receiver(sender, instance, created, *args, **kwargs):
 
 		# send hashtag signal to user here.
 
-post_save.connect(tweet_save_receiver, sender=Tweet)	
+post_save.connect(tweet_save_receiver, sender=Tweet) 
